52563f457231e714b66378b1ab93fd8111f181e6
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-12 20:52:56 +0600

    added AuthProvider for firebase authentication

---
+++ src/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,87 @@
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import auth from "../config/firebase";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const signUpEmail = (email, password) => {
+    setIsLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInEmail = (email, password) => {
+    setIsLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleProvider = new GoogleAuthProvider();
+  const signInGoogle = () => {
+    setIsLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const githubProvider = new GithubAuthProvider();
+  const signInGithub = () => {
+    setIsLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  const updateUser = (name, imageUrl) => {
+    setIsLoading(true);
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: imageUrl,
+    });
+  };
+
+  const logOut = () => {
+    setUser(null);
+    return signOut(auth);
+  };
+
+  const authData = {
+    user,
+    setUser,
+    isLoading,
+    setIsLoading,
+    signUpEmail,
+    signInEmail,
+    signInGoogle,
+    signInGithub,
+    updateUser,
+    logOut,
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("Observing...");
+      setUser(user);
+      setIsLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
