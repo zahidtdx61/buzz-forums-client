c05c99d4d207dbae03ad857167d9f2239805da54
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-12 23:48:40 +0600

    chore: Add user menu to UserInfo component

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,13 +1,15 @@
-import { Avatar } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Avatar, Menu, MenuItem, Typography } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import { Tooltip } from "react-tooltip";
 import useAuth from "../../hooks/useAuth";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const UserInfo = () => {
   const { user, logOut } = useAuth();
-  const { displayName, photoURL } = user;
+  const { displayName, photoURL } = user || {};
   const axiosSecure = useAxiosSecure();
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
   const handleSignOut = async () => {
     try {
@@ -19,22 +21,55 @@ const UserInfo = () => {
     }
   };
 
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const settings = ["Profile", "Dashboard"];
+
   return (
     <>
       <div className="flex items-center justify-end gap-3">
         <button
           data-tooltip-id="my-tooltip"
           data-tooltip-content={displayName || "Not available"}
+          onClick={handleOpenUserMenu}
         >
           <Avatar src={photoURL} />
         </button>
         <Tooltip id="my-tooltip" />
-        <button
-          onClick={handleSignOut}
-          className="px-5 py-2 bg-primary-maroon text-slate-50 rounded hover:bg-opacity-70 hover:scale-105 hidden lg:block"
+
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
         >
-          <NavLink to={"/"}>Log Out</NavLink>
-        </button>
+          {settings.map((setting) => (
+            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              <Link to={`/${setting.toLowerCase()}`}>
+                <Typography textAlign="center">{setting}</Typography>
+              </Link>
+            </MenuItem>
+          ))}
+          <MenuItem onClick={handleSignOut}>
+            <Typography textAlign="center">Logout</Typography>
+          </MenuItem>
+        </Menu>
       </div>
     </>
   );
