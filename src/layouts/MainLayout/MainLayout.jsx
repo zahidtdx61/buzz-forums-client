772aff437be2e972b37e970bcc36ab9c54a1b612
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-12 20:50:11 +0600

    added firebase config

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
+      <div className="w-full h-20">
+        <Navbar />
+      </div>
       <Outlet />
     </div>
   );
