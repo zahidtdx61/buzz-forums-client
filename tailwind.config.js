2fc5391a8870b119279f905d869dd6f762d1944e
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-12 23:10:58 +0600

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend", "sans-serif"],
+        "fira-sans": ["Fira Sans", " sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+      colors: {
+        "primary-blue": "#2C4E80",
+        "primary-sky": "#0E46A3",
+        "primary-navy": "#1E0342",
+        "primary-maroon": "#DD5746",
+      },
+    }
   },
   plugins: [],
-}
+};
