9c1a4c191907a16d751605b91a1a12221ce3a39a
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-19 23:00:03 +0600

    feat: Add form to add tags for posts on AdminProfile

--- src/pages/MyProfile/AdminProfile.jsx
+++ src/pages/MyProfile/AdminProfile.jsx
@@ -1,12 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
-import useAxiosSecure from "../../hooks/useAxiosSecure";
 import LoadContent from "../../components/Loader/LoadContent";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 import AdminPieChart from "./AdminPieChat";
+import { useForm } from "react-hook-form";
 
 const AdminProfile = () => {
   const axiosSecure = useAxiosSecure();
+  const { register, handleSubmit, reset } = useForm();
 
-  const { data: siteData, isLoading:statsLoading } = useQuery({
+  const handleFormData = async (data) => {
+    console.log(data);
+    try {
+      const response = await axiosSecure.post("/admin/add-tag", data);
+      console.log(response.data);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const { data: siteData, isLoading: statsLoading } = useQuery({
     queryKey: ["site-stats"],
     queryFn: async () => {
       const response = await axiosSecure.get("/admin/all-users-data");
@@ -14,7 +27,7 @@ const AdminProfile = () => {
     },
   });
 
-  if(statsLoading) return <LoadContent />;
+  if (statsLoading) return <LoadContent />;
 
   return (
     <div className="w-[95%] lg:max-w-screen-lg mx-auto mb-4">
@@ -42,7 +55,52 @@ const AdminProfile = () => {
         </div>
       </div>
 
-      <AdminPieChart  siteData={siteData} />
+      <AdminPieChart siteData={siteData} />
+
+      <div className="my-12 font-mulish">
+        <h1 className="text-slate-700 text-xl font-semibold">
+          Add Tags for Posts
+        </h1>
+        <p className="text-slate-600 text-base">
+          Add tags to posts to make them more searchable.
+        </p>
+
+        <form
+          className="space-y-5 mb-8 mt-2 mx-auto text-sm"
+          onSubmit={handleSubmit(handleFormData)}
+        >
+          <div>
+            <label className="font-medium">Add a Tag Title</label>
+            <input
+              type="text"
+              required
+              {...register("value")}
+              placeholder={"Add your tag title"}
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-600 shadow-sm rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label className="font-medium">Add a short name or Short description for the tag</label>
+            <input
+              type="text"
+              required
+              {...register("label")}
+              placeholder="Add your tag description"
+              className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-600 shadow-sm rounded-lg"
+            />
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:cursor-not-allowed disabled:bg-zinc-300 disabled:text-gray-500"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
