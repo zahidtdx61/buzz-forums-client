4611a9c7c702dafaaf1499abd3e0932f5f0efb74
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-19 13:30:50 +0600

    feat: Add report functionality to CommentContent component

--- src/components/CommentContent/CommentContent.jsx
+++ src/components/CommentContent/CommentContent.jsx
@@ -1,11 +1,37 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 import Select from "react-select";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import LoadContent from "../Loader/LoadContent";
 import SeeCommentModal from "./SeeCommentModal";
+import toast from "react-hot-toast";
 
 const CommentContent = ({ comment }) => {
+  const axiosSecure = useAxiosSecure();
   const [feedback, setFeedback] = useState("");
   const [seeComment, setSeeComment] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const reportComment = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axiosSecure.post(
+        `/user/comment/report/${comment._id}`,
+        { feedback }
+      );
+      console.log(response.data);
+      setIsLoading(false);
+      setFeedback("");
+      toast.success("Comment Reported Successfully");
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      toast.error("Error Reporting Comment");
+    } finally {
+      setIsLoading(false);
+      setFeedback("");
+    }
+  };
 
   const options = [
     { value: "bad", label: "Bad Comment" },
@@ -13,13 +39,45 @@ const CommentContent = ({ comment }) => {
     { value: "bully", label: "Bully Me" },
   ];
 
+  if (isLoading)
+    return (
+      <tr>
+        <td>
+          <LoadContent />
+        </td>
+        <td>
+          <LoadContent />
+        </td>
+        <td>
+          <LoadContent />
+        </td>
+        <td>
+          <LoadContent />
+        </td>
+        <td>
+          <LoadContent />
+        </td>
+      </tr>
+    );
+
   return (
     <tr>
       <td className="p-2 text-left">
         {comment.comment.slice(0, Math.min(20, comment.comment.length))}
         {comment.comment.length > 20 ? "..." : ""}
-        {comment.comment.length > 20 && (<button className="text-sm text-blue-500" onClick={() => setSeeComment(true)}>See More</button>)}
-        <SeeCommentModal open={seeComment} setOpen={setSeeComment} comment={comment} />
+        {comment.comment.length > 20 && (
+          <button
+            className="text-sm text-blue-500"
+            onClick={() => setSeeComment(true)}
+          >
+            See More
+          </button>
+        )}
+        <SeeCommentModal
+          open={seeComment}
+          setOpen={setSeeComment}
+          comment={comment}
+        />
       </td>
       <td className="p-2">{new Date(comment.createdAt).toLocaleString()}</td>
       <td className="p-2">{comment?.userId?.email}</td>
@@ -29,6 +87,7 @@ const CommentContent = ({ comment }) => {
       <td className="p-2">
         <button
           className="text-white bg-red-600 px-5 py-1 rounded-md disabled:cursor-not-allowed disabled:bg-red-300 disabled:text-gray-800"
+          onClick={reportComment}
           disabled={feedback.length === 0}
         >
           Report
