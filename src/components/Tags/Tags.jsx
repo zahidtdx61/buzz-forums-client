ef6783c6666e503e6d7e431b1d240b71f937be40
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-20 14:19:41 +0600

    feat: Add Tags component for displaying and selecting tags

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import LoadContent from "../Loader/LoadContent";
+
+const Tags = ({ tag, setTag, allTags, tagsLoading }) => {
+  if (tagsLoading) return <LoadContent />;
+
+  return (
+    <div className="max-w-screen-lg mx-auto my-8">
+      <div className="flex gap-2 flex-wrap justify-center">
+        {allTags?.map((tagItem) => (
+          <button
+            key={tagItem._id}
+            onClick={() => setTag(tagItem.value)}
+            className={`px-4 py-2 rounded ${
+              tag === tagItem.value
+                ? "bg-blue-500 text-white"
+                : "bg-blue-200 text-blue-800"
+            }`}
+          >
+            #{tagItem.label}
+          </button>
+        ))}
+        <button onClick={() => setTag(null)} className={`px-4 py-2 rounded bg-red-200 text-blue-800`}>
+            Clear tags
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Tags.propTypes = {
+  tag: PropTypes.string,
+  setTag: PropTypes.func,
+  allTags: PropTypes.array,
+  tagsLoading: PropTypes.bool,
+};
+
+export default Tags;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import Tags from "../../components/Tags/Tags";
+import useAxiosCommon from "../../hooks/useAxiosCommon";
 
 const Home = () => {
+  const [tag, setTag] = useState(null);
+  const axios = useAxiosCommon();
+
+  const { data: allTags, isLoading: tagsLoading } = useQuery({
+    queryKey: ["tags"],
+    queryFn: async () => {
+      const response = await axios.get("/user/tags");
+      console.log(response.data.data);
+      return response?.data?.data;
+    },
+  });
+
   return (
     <>
       <Helmet>
         <title>Buzz Forums | Home</title>
       </Helmet>
-      <div>Home</div>
+      <div className="min-h-[calc(100vh-80px)] mx-auto w-full mt-12 p-2">
+        <Tags
+          tag={tag}
+          setTag={setTag}
+          allTags={allTags}
+          tagsLoading={tagsLoading}
+        />
+      </div>
     </>
   );
 };
