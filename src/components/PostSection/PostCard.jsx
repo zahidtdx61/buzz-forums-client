a45f758d6189832c8e69465cd014b22286de2ad0
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-20 17:46:09 +0600

    feat: Add PostCard component for displaying individual posts

---
+++ src/components/PostSection/PostCard.jsx
@@ -0,0 +1,65 @@
+import { Comment } from "@mui/icons-material";
+import PropTypes from "prop-types";
+import { BiDownvote, BiUpvote } from "react-icons/bi";
+import { Link } from "react-router-dom";
+
+const PostCard = ({ post }) => {
+  return (
+    <Link
+      to={`/post/${post._id}`}
+      key={post._id}
+      className="bg-zinc-100 p-4 rounded w-full"
+    >
+      <h1 className="text-base font-semibold text-slate-800">{post.title}</h1>
+      <div className="w-fit mt-1 text-sm bg-zinc-300 text-slate-700 px-2 rounded">
+        #{post.tag}
+      </div>
+
+      <div className="mt-8 flex flex-col md:flex-row md:justify-between">
+        {/* author and post date */}
+        <div className="flex gap-2 items-center">
+          <div className="size-8">
+            <img
+              src={post.user.image}
+              alt={post.user.name}
+              className="rounded-full"
+            />
+          </div>
+          <div className="text-slate-600 text-sm font-semibold">
+            <p>
+              Author name: <span className="font-normal">{post.user.name}</span>
+            </p>
+            <p>
+              Posted at:{" "}
+              <span className="font-normal">
+                {new Date(post.createdAt).toLocaleString()}
+              </span>
+            </p>
+          </div>
+        </div>
+
+        {/* comments and votes */}
+        <div className="flex gap-3">
+          <p className="text-slate-600 text-sm font-semibold mt-2 flex gap-1">
+            <Comment />
+            <span className="font-normal">{post.comments.length}</span>
+          </p>
+          <p className="text-slate-600 text-sm font-semibold mt-2 flex gap-1">
+            <BiUpvote size={18} />
+            <span className="font-normal">{post.upVotes}</span>
+          </p>
+          <p className="text-slate-600 text-sm font-semibold mt-2 flex gap-1">
+            <BiDownvote size={18} />
+            <span className="font-normal">{post.downVotes}</span>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.object,
+};
+
+export default PostCard;
--- src/components/PostSection/PostSection.jsx
+++ src/components/PostSection/PostSection.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 import LoadContent from "../Loader/LoadContent";
-import { Link } from "react-router-dom";
+import PostCard from "./PostCard";
 
 const PostSection = ({ allPosts, isLoading }) => {
   if (isLoading) return <LoadContent />;
   return (
-    <div className="w-[95%] lg:max-w-screen-lg mx-auto my-12 ">
+    <div className="w-[95%] lg:max-w-screen-md mx-auto my-16 ">
       <h1 className="text-2xl font-semibold text-slate-800 text-center mb-4">
         Popular Posts
       </h1>
@@ -20,16 +20,7 @@ const PostSection = ({ allPosts, isLoading }) => {
       {allPosts.length !== 0 && (
         <div className="mx-auto flex flex-col gap-2">
           {allPosts.map((post) => (
-            <Link
-              to={`/post/${post._id}`}
-              key={post._id}
-              className="bg-zinc-100 p-4 rounded border-l-4 border-slate-600"
-            >
-              <h1 className="text-base font-semibold text-slate-800">
-                {post.title}
-              </h1>
-              <p className="text-slate-600 mt-2">{post.description}</p>
-            </Link>
+            <PostCard key={post._id} post={post} />
           ))}
         </div>
       )}
