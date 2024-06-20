1f5d87ac5b6d5c9f4206ff73a2b16e2be60ab2cc
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-06-20 12:40:41 +0600

    refactor: Update disabled state styling for voting buttons

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -124,7 +124,7 @@ const PostDetails = () => {
             <button
               disabled={voteLoading}
               onClick={() => handleVote("up")}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-8"
+              className="bg-blue-500 text-white px-4 py-2 rounded mt-8 disabled:cursor-not-allowed"
             >
               <BiUpvote />
             </button>
@@ -134,7 +134,7 @@ const PostDetails = () => {
             <button
               disabled={voteLoading}
               onClick={() => handleVote("down")}
-              className="bg-blue-500 text-white px-4 py-2 rounded mt-8 ml-4"
+              className="bg-blue-500 text-white px-4 py-2 rounded mt-8 ml-4 disabled:cursor-not-allowed"
             >
               <BiDownvote />
             </button>
